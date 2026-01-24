56edb4c8d5a56dd266978eb481c476c700703015
Ruben Marques <rubenrfhm98@gmail.com>
2026-01-24 11:26:03 +0000

    Initial Commit

---
+++ Statly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Statly
+//
+//  Created by Ruben Marques on 24/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Statly/StatlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatlyApp.swift
+//  Statly
+//
+//  Created by Ruben Marques on 24/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct StatlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
